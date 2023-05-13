4a61c38b0ce41b33b1c84410bf31293f23c8b365
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-05-13 20:48:10 +0200

    init commit (scaffold basic configuration)

---
+++ src/test/java/pl/miloszgilga/airhubmasterserver/AirHubMasterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.miloszgilga.airhubmasterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirHubMasterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
