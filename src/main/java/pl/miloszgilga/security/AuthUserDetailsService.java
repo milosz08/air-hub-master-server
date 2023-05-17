f4a373a337ec8ef955b925e73e2007058d4f5893
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-05-18 01:36:58 +0200

    create basic spring security configuration

---
+++ src/main/java/pl/miloszgilga/security/AuthUserDetailsService.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <http://miloszgilga.pl>
+ *
+ * File name: AuthUserDetailsService.java
+ * Last modified: 17/05/2023, 20:19
+ * Project name: air-hub-master-server
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+package pl.miloszgilga.security;
+
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import pl.miloszgilga.domain.user.IUserRepository;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@Service
+public class AuthUserDetailsService implements UserDetailsService {
+
+    private final IUserRepository userRepository;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    AuthUserDetailsService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
