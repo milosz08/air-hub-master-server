a33692aa7dcca4f227b52b33fdd042487c929985
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-05-19 23:22:39 +0200

    create utilities class

---
+++ src/main/java/pl/miloszgilga/utils/Utilities.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <http://miloszgilga.pl>
+ *
+ * File name: Utilities.java
+ * Last modified: 19/05/2023, 14:27
+ * Project name: air-hub-master-server
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+package pl.miloszgilga.utils;
+
+import pl.miloszgilga.domain.user.UserEntity;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class Utilities {
+
+    private Utilities() {
+    }
+
+    public static String parseFullName(UserEntity user) {
+        return user.getFirstName() + " " + user.getLastName();
+    }
+}
