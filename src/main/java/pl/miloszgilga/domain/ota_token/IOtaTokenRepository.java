adab5510973e38b472f6d77bb9e2f1810e99e7b9
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-05-27 02:49:45 +0200

    minor changes

--- src/main/java/pl/miloszgilga/domain/ota_token/IOtaTokenRepository.java
+++ src/main/java/pl/miloszgilga/domain/ota_token/IOtaTokenRepository.java
@@ -18,10 +18,10 @@
 
 package pl.miloszgilga.domain.ota_token;
 
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
