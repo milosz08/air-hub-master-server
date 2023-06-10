5ba9cee73c65ab70872213d6ee4b5fd892b36399
milosz08 <gilgamilosz451@gmail.com>
2023-06-10 22:02:14 +0200

    fixed: normal bad request exceptions returns 401 status code

--- src/main/java/pl/miloszgilga/exception/AccountException.java
+++ src/main/java/pl/miloszgilga/exception/AccountException.java
@@ -21,7 +21,7 @@ package pl.miloszgilga.exception;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.HttpStatus;
-import org.jmpsl.core.exception.RestServiceAuthServerException;
+import org.jmpsl.core.exception.RestServiceServerException;
 
 import pl.miloszgilga.i18n.AppLocaleSet;
 
@@ -29,7 +29,7 @@ import pl.miloszgilga.i18n.AppLocaleSet;
 
 public class AccountException {
 
-    @Slf4j public static class LoginAlreadyExistException extends RestServiceAuthServerException {
+    @Slf4j public static class LoginAlreadyExistException extends RestServiceServerException {
         public LoginAlreadyExistException(String newLogin) {
             super(HttpStatus.BAD_REQUEST, AppLocaleSet.LOGIN_ALREADY_EXIST_EXC);
             log.error("Attempt to change login '{}' on account, when it is already existing on another account.",
@@ -37,7 +37,7 @@ public class AccountException {
         }
     }
 
-    @Slf4j public static class EmailAddressAlreadyExistException extends RestServiceAuthServerException {
+    @Slf4j public static class EmailAddressAlreadyExistException extends RestServiceServerException {
         public EmailAddressAlreadyExistException(String newEmail) {
             super(HttpStatus.BAD_REQUEST, AppLocaleSet.EMAIL_ADDRESS_ALREADY_EXIST_EXC);
             log.error("Attempt to change email '{}' on account, when it is already existing on another account.",
