eea78736a948c868a83678d1861aafd8ffdb7602
milosz08 <gilgamilosz451@gmail.com>
2023-06-27 04:08:21 +0200

    create workshop network section

---
+++ src/main/java/pl/miloszgilga/network/workshop/WorkshopController.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <http://miloszgilga.pl>
+ *
+ * File name: ServiceController.java
+ * Last modified: 6/27/23, 3:05 AM
+ * Project name: air-hub-master-server
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+package pl.miloszgilga.network.workshop;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import org.jmpsl.security.user.AuthUser;
+import org.jmpsl.security.user.CurrentUser;
+
+import pl.miloszgilga.network.workshop.resdto.WorkshopPlaneStatsResDto;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/workshop")
+public class WorkshopController {
+
+    private final IWorkshopService workshopService;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @GetMapping("/planes")
+    ResponseEntity<List<WorkshopPlaneStatsResDto>> getPlanes(@CurrentUser AuthUser user) {
+        return new ResponseEntity<>(workshopService.getPlanes(user), HttpStatus.OK);
+    }
+}
